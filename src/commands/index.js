68d2e6ffa1d9f6af3438ad36e32bbbbfe3f601b9
unsnruu <unsnruu@gmail.com>
2025-10-12 17:15:08 +0900

    :bug: fix: /checkin 명령어 등록 누락 수정
    - commands/index.js의 명령어 목록에 /checkin 명령어를 추가합니다.
    
    - 이전에 누락되었던 명령어 등록 문제를 해결합니다.

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,5 +1,6 @@
 import ping from "./ping.js";
 import dailyGoal from "./dailyGoal.js";
+import checkin from "./checkin.js";
 
 // 활성화할 명령어를 이 배열에 추가합니다.
-export const commands = [ping, dailyGoal];
+export const commands = [ping, dailyGoal, checkin];
