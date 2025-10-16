21f4f07c147315d0568c59a606cf86b744b247c5
unsnruu <unsnruu@gmail.com>
2025-10-16 17:29:31 +0900

    feat: schedules 메시지 및 주석 수정
    - dailyCheckinThread, dailyGoalThread 메시지에서 불필요한 `\n` 제거
    - weeklyAchievementThread의 스케줄 주석 오류 수정
    - weeklyAchievementThread의 메시지 내용 변경

--- src/schedules/weeklyAchievementThread.js
+++ src/schedules/weeklyAchievementThread.js
@@ -1,8 +1,8 @@
 const channelId = process.env.WEEKLY_ACHIEVEMENT_CHANNEL_ID;
 
 export const weeklyAchievementThreadJob = {
-  // 매주 금요일 오후 4시
-  schedule: "0 16 * * 5",
+  // 매주 금요일 오전 10시
+  schedule: "00 10 * * 5",
   async task(client) {
     console.log("⏰ 주간 스레드 생성 작업을 시작합니다.");
     try {
@@ -18,7 +18,9 @@ export const weeklyAchievementThreadJob = {
       const thread = await channel.threads.create({
         name: `📅 ${dateString} | 한 주간의 성과를 공유해주세요!`,
       });
-      await thread.send({ content: `@everyone 한 주간의 성과를 공유하며 마무리하는 시간입니다! 📅` });
+      await thread.send({
+        content: `@everyone 한주의 성과를 공유하는 마무리. 미리 Figma 링크를 공유해주세요 :)`,
+      });
       console.log(`✅ ${thread.name} 스레드를 성공적으로 생성했습니다.`);
     } catch (error) {
       console.error("스레드 생성 중 오류가 발생했습니다:", error);
